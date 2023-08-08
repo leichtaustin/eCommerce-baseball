1ef922cb43445bf82f83ba13162d06d436c316a5
Austin Leicht <leichtaustin@gmail.com>
2023-08-08 14:06:11 -0600

    Added initial test file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { useDispatch } from 'react-redux';
-import { PlayerCard } from './components/Player_Card/Player_Card';
 import { SearchResults } from './components/SearchResults/SearchResults';
 import { playerFactory } from './utility/dataFactory';
 import { addPlayerCards } from './components/SearchResults/searchResultsSlice';
---
+++ src/components/__tests__/testMain.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import App from '../../App'
+import { render, screen } from '@testing-library/react';
+
+
+test('sanity check', () => {
+    expect(true).toBe(true);
+});
