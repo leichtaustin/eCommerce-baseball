db096820beb7fb401f5516cad5873b9ce2954aab
Austin Leicht <leichtaustin@gmail.com>
2023-08-11 16:10:54 -0600

    Added tests for cart slice

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -12,7 +12,7 @@ export const Filters = () => {
     const handleFormSubmit = (e) => {
         let filterArray = [];
         e.preventDefault();
-        let formOutput = {
+        let filterOutput = {
             Outfield: document.getElementById('outfield').checked,
             Shortstop: document.getElementById('shortstop').checked,
             StartingPitcher: document.getElementById('starting_pitcher').checked,
@@ -23,9 +23,9 @@ export const Filters = () => {
             Catcher: document.getElementById('catcher').checked,
             DesignatedHitter: document.getElementById('designated_hitter').checked,
         }
-        dispatch(updateFilter(formOutput));
-        Object.keys(formOutput).forEach((key) => {
-            filterArray.push({position: key, value: formOutput[key]})
+        dispatch(updateFilter(filterOutput));
+        Object.keys(filterOutput).forEach((key) => {
+            filterArray.push({position: key, value: filterOutput[key]})
         });
         
         //reset the filtered players and then reapply filters
@@ -46,33 +46,33 @@ export const Filters = () => {
     return (
         <div className='filterContainer'>
             <h2>Filters: </h2>
-            <form onSubmit={handleFormSubmit}>
-                <input type='checkbox' id='outfield' name='outfield'/>
-                <label for='outfield'>OF</label>
+            <form onSubmit={handleFormSubmit} data-testid='filterTest'>
+                <input type='checkbox' id='outfield' name='outfield' data-testid='oftest'/>
+                <label htmlFor='outfield'>OF</label>
                 <br/>
                 <input type='checkbox' id='shortstop' name='shortstop'/>
-                <label for='shortstop'>SS</label>
+                <label htmlFor='shortstop'>SS</label>
                 <br/>
                 <input type='checkbox' id='starting_pitcher' name='starting_pitcher'/>
-                <label for='starting_pitcher'>SP</label>
+                <label htmlFor='starting_pitcher'>SP</label>
                 <br/>
                 <input type='checkbox' id='relief_pitcher' name='relief_pitcher'/>
-                <label for='relief_pitcher'>RP</label>
+                <label htmlFor='relief_pitcher'>RP</label>
                 <br/>
                 <input type='checkbox' id='first' name='first'/>
-                <label for='first'>1B</label>
+                <label htmlFor='first'>1B</label>
                 <br/>
                 <input type='checkbox' id='second' name='second'/>
-                <label for='second'>2B</label>
+                <label htmlFor='second'>2B</label>
                 <br/>
                 <input type='checkbox' id='third' name='third'/>
-                <label for='third'>3B</label>
+                <label htmlFor='third'>3B</label>
                 <br/>
                 <input type='checkbox' id='catcher' name='catcher'/>
-                <label for='catcher'>C</label>
+                <label htmlFor='catcher'>C</label>
                 <br/>
                 <input type='checkbox' id='designated_hitter' name='designated_hitter'/>
-                <label for='designated_hitter'>DH</label>
+                <label htmlFor='designated_hitter'>DH</label>
                 <br/>
                 <br/>
                 <button type='submit'>Apply Filters</button>              
--- src/components/__tests__/testMain.js
+++ src/components/__tests__/testMain.js
@@ -1,18 +1,75 @@
 import React from 'react';
 import App from '../../App'
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { setupStore } from '../../store';
 import { renderWithProviders } from '../../utility/testUtils';
+import { Header } from '../Header/Header';
+import { Filters } from '../Filters/Filters';
+import playerFiltersReducer, { updateFilter } from '../Filters/FiltersSlice';
+import { selectPlayerFilters } from '../Filters/FiltersSlice';
+import CartSliceReducer, { addToCart } from '../Cart/CartSlice';
+import { Cart } from '../Cart/Cart';
+
 
 
 test('sanity check', () => {
     expect(true).toBe(true);
 });
 
-test('render searchBar', () => {
-    const store = setupStore()
+describe('<Header />', () => {
+    it('renders title of page', () => {
+        const store = setupStore()
 
-    renderWithProviders(<App />, { store })
-    const titleElement = screen.getByTestId('title-element');
-    expect(titleElement).toBeInTheDocument();
+        renderWithProviders(<Header />, {store})
+        const title = screen.getByTestId('title-element');
+        expect(title).toHaveTextContent('Team Auction');
+    })
+})
+
+describe('<Filters />', () => {
+    it('renders filter element', () => {
+        const store = setupStore();
+        
+        renderWithProviders(<Filters />, {store} )
+        const filters = screen.getByTestId('filterTest');
+        expect(filters).toBeInTheDocument;
+    })
+
+    // it('updates filter state on form submit', () => {
+    //     const previoiusState = {};
+    //     const store = setupStore();
+    //     renderWithProviders(<Filters />, {store} );
+    //     fireEvent.click(screen.getByTestId('oftest'))
+
+    //     let formOutput = {
+    //         Outfield: screen.getAllByTestId('oftest').value
+    //     }
+    //     //fireEvent.click(screen.getByText('Apply Filters'));
+
+    //     expect(playerFiltersReducer(previoiusState, updateFilter(formOutput))).toEqual({
+    //         Outfield: true,
+    //     })
+    // })
+})
+
+describe('<Cart />', () => {
+    it('Adds player value to total value', () => {
+        const previousState = {
+            playersInCart: [],
+            cartTotal: 0,
+        }
+
+        expect(CartSliceReducer(previousState, addToCart({firstName: 'Thomas', lastName: 'Turner', auctionValue: 46}))).toEqual(
+            {
+                playersInCart: [{firstName: 'Thomas', lastName: 'Turner', auctionValue: 46, isInCart: true,}],
+                cartTotal: 46,
+            }
+        )
+    })
+    it('renders cart total on screen', () => {
+        const store = setupStore();
+        renderWithProviders(<Cart/>, {store});
+        addToCart({firstName: 'Thomas', lastName: 'Turner', auctionValue: 46})
+        expect("46").toBeInTheDocument;
+    })
 })
